ec767a0b482235701a88579cdc680032d259274c
Niferu <wargacki.patryq@gmail.com>
2023-01-22 17:57:33 +0100

    getLendingPool function added

--- hardhat.config.js
+++ hardhat.config.js
@@ -33,13 +33,19 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: "0.8.8",
+                version: "0.4.19",
+            },
+            {
+                version: "0.6.0",
+            },
+            {
+                version: "0.6.6",
             },
             {
                 version: "0.6.12",
             },
             {
-                version: "0.4.19",
+                version: "0.8.8",
             },
         ],
     },
--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,7 +1,25 @@
 const { getWeth, AMOUNT } = require("../scripts/getWeth.js")
+const { getNamedAccounts, ethers, network } = require("hardhat")
+const { networkConfig } = require("../helper-hardhat-config")
 
 async function main() {
     await getWeth()
+    const { deployer } = getNamedAccounts()
+    await getLendingPool(deployer)
+}
+
+async function getLendingPool(account) {
+    // We are getting "lendingPoolAddressesProvider" from:
+    // Run command: `yarn add --dev @aave/protocol-v2`
+    // node_modules -> @aave/protocol-v2/contracts/interfaces/ILendingPoolAddressesProvider.sol:ILendingPoolAddressesProvider
+    const lendingPoolAddressesProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        networkConfig[network.config.chainId].lendingPoolAddressesProvider,
+        account
+    )
+    const lendingPoolAddress = await lendingPoolAddressesProvider.getLendingPool()
+    const lendingPool = await ethers.getContractAt("ILendingPool", lendingPoolAddress, account)
+    return lendingPool
 }
 
 main()
